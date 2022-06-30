96bf70d20db942a3cd88488c17ec31a17b661f2f
EderBasso <eder.sdz@gmail.com>
2022-06-29 22:04:55 -0300

    Aula Stream

--- src/Contato.java
+++ src/Contato.java
@@ -2,9 +2,12 @@ public class Contato{
     private String nome;
     private String telefone;
 
-    public Contato(String nome, String telefone) {
+    private boolean ativo;
+
+    public Contato(String nome, String telefone, boolean ativo) {
         this.nome = nome;
         this.telefone = telefone;
+        this.ativo = ativo;
     }
 
     public String getNome() {
@@ -13,6 +16,10 @@ public class Contato{
 
     public String getTelefone() {
         return telefone;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.function.Predicate;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ArrayList<Contato> agendaGenerics = new ArrayList<>();
-        agendaGenerics.add(new Contato("Marcia", "12312412"));
-        agendaGenerics.add(new Contato("Nicole", "4483392"));
-        agendaGenerics.add(new Contato("Odete", "8234723847"));
+        agendaGenerics.add(new Contato("Marcia", "12312412", true));
+        agendaGenerics.add(new Contato("Nicole", "4483392", true));
+        agendaGenerics.add(new Contato("Odete", "8234723847", false));
 
         Character letra = 'n';
-        filtrarAgendaGenerics(agendaGenerics, t -> t.getNome().toLowerCase().charAt(0) == letra);
+        //filtrarAgendaGenerics(agendaGenerics, t -> t.getNome().toLowerCase().charAt(0) == letra);
+
+        agendaGenerics.stream()
+                .filter(e -> e.isAtivo())
+                .forEach(e -> System.out.println(e));
+
+        var fileReader = new FileReader( "relianceTrendsFashion.csv", StandardCharsets.UTF_8);
+        try(BufferedReader reader = new BufferedReader(fileReader)){
+            reader.lines()
+                    .filter(t -> t.contains("Women"))
+                    .filter(t-> t.contains("Fusionwear"))
+                    .forEach(l -> {
+                        System.out.println("---");
+                        Arrays.stream(l.split(","))
+                                .forEach(t -> {
+                                    System.out.print(t);
+                                    System.out.print(" - ");
+                                });
+                        System.out.println();
+                    });
+        }
     }
 
     public static <T> void filtrarAgendaGenerics (ArrayList<T> agenda, Predicate<T> tester){
